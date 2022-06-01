37e19d8f906fc3b8f1a0420681f9e4802431ead3
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2022-06-01 20:51:17 +1000

    time dependent rho

--- workshop - track rho.R
+++ workshop - track rho.R
@@ -1,6 +1,5 @@
 ### To have a dynamic rho need:
-# (1) estimate of max rho
-# (2) estimate of shape of waning
+# (1) shape of rho waning
 # (3) tracking of days since infection of people in Recovered class
 
 #Assumptions log:
@@ -14,39 +13,37 @@
 # at each time-step sum incidence 0-179 days
 
 
-### (1) Estimate of max rho ###########################################################################
-#COMEBACK - lit review
-max_rho = 0.95
+### (1) Shape of waning ###############################################################################
+#See rho.xlsx in parameter estimation
+rho_dn = crossing(strain = 'omicron',
+                                   outcome = c('symptomatic_disease','severe_outcome'),
+                                   days = seq(0,365*2)
+                                   )
+rho_dn = rho_dn %>% mutate(
+  protection = case_when(
+    outcome == 'symptomatic_disease' ~ 0.9932*exp(-0.004*days),
+    outcome == 'severe_outcome' ~ 0.8859*exp(-0.0002*days)
+  )
+)
+
+rho_est_plots = list()
+rho_est_plots[[1]] = ggplot() + geom_line(data = rho_dn[rho_dn$outcome == 'symptomatic_disease',],
+                     aes(days,protection)) + ylim(0,1) +labs(title = 'symptomatic_disease')
+rho_est_plots[[2]] = ggplot() + geom_line(data = rho_dn[rho_dn$outcome == 'severe_outcome',],
+                     aes(days,protection)) + ylim(0,1) +labs(title = 'severe_outcome')
+
+grid.arrange(rho_est_plots[[1]], rho_est_plots[[2]])
 #######################################################################################################
 
 
 
-### (2) Shape of waning ###############################################################################
-#COMEBACK - lit review
-#quick look
-ggplot(immunity_from_infection[immunity_from_infection$outcome == 'acquisition',]) + geom_line(aes(days,ve_predict_mean,color=strain))
-
-workshop <- immunity_from_infection[immunity_from_infection$outcome == 'acquisition',] %>%
-  group_by(strain) %>%
-  mutate(waning = ve_predict_mean / max(ve_predict_mean))
-
-ggplot(workshop) + geom_line(aes(days,waning,color=strain))
-#diff shape!
-
-rho_dn = workshop[workshop$strain == strain_now,] %>%
-  mutate(rho_est = max_rho * waning) %>% 
-  ungroup() %>%
-  select(days,rho_est)
-ggplot(rho_dn) + geom_line(aes(days,rho_est)) + ylim(0,1)
-#######################################################################################################
-
-
-
-### (3) Tracking of days since infection (proxy) for people in Recovered class ########################
+### (2) Tracking of days since infection (proxy) for people in Recovered class ########################
 hist_cases = case_history %>% mutate(daily_cases = new * underascertainment_est) %>%
   select(date,daily_cases)
 
-rho_time_step <- function(strain_now,date_now){
+rho_time_step <- function(this_outcome,date_now){
+  
+  this_rho = rho_dn %>% filter(outcome == this_outcome)
   
   workshop = incidence_log %>% select(date,daily_cases)
   workshop = rbind(workshop,hist_cases)
@@ -59,9 +56,11 @@ rho_time_step <- function(strain_now,date_now){
   
   ggplot(workshop) + geom_line(aes(date,prop_window))  
   
+  if (sum(workshop$prop_window) != 1){stop('error in rho_time_step')}
+  
 
-  workshop = workshop %>% left_join(rho_dn) %>%
-    mutate(interim = rho_est * prop_window)
+  workshop = workshop %>% left_join(this_rho) %>%
+    mutate(interim = protection * prop_window)
   
   return(sum(workshop$interim))
 }
