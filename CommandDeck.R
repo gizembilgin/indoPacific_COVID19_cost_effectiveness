238e15fbf8582792ee9cdb5e112ff5915fd3904e
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2022-09-12 11:42:38 +1000

    sensitivity analysis configuration
    only refit for sensitivity analysis if vaccine hesitance difference - no need to refit for VE reduction since only effects VE against severe outcomes
    
    nestle checking if statements to ensure variable exists

--- CommandDeck.R
+++ CommandDeck.R
@@ -48,7 +48,7 @@ if ( debug == "on"){
   plotting = "on"
   
 
-  RR_estimate = RR_default = 2
+  RR_estimate  = 2
   vax_strategy_toggle = "on"
   vax_risk_strategy_toggle = "on"
   risk_group_toggle = "on" 
@@ -84,7 +84,7 @@ if ( debug == "on"){
 if (fitting == "on"){
   warning('Fitting is on')
   if (debug == "on"){debug = "off"}
-} else{
+} else if ( ! 'vax_hesistancy_risk_group' %in% names(sensitivity_analysis_toggles)){
   load(file = '1_inputs/last_fit_date.Rdata')
   if (as.numeric(abs(fitted_max_date - Sys.Date()))>30){ 
     warning('refitted')
@@ -108,9 +108,26 @@ if (fitting == "on"){
     fitted_incidence_log_tidy = fitted_incidence_log_tidy %>% filter(date <= date_start) # CHECKED last of fitted log = first of new log
     fitted_incidence_log = fitted_incidence_log %>% filter(date <= date_start)
     
-    if ((is.na(risk_group_prioritisation_to_date) == FALSE) | (! default_prioritisation_proportion == 0.5) | (!VE_at_risk_suppress == 1)){
-      stop('change fitting to this change in risk group characteristic')
+    if (risk_group_toggle == "on"){
+      if ((is.na(risk_group_prioritisation_to_date) == FALSE) | (! default_prioritisation_proportion == 0.5) | (!VE_at_risk_suppress == 1)){
+        stop('change fitting to this change in risk group characteristic')
+      }
     }
+  }
+} else{
+  if ('vax_hesistancy_risk_group' %in% names(sensitivity_analysis_toggles)){
+    
+    if (! risk_group_name == 'pregnant_women'){stop('havent configured vax hesistance sensitivity analysis for another risk group')}
+    
+    load(file = '1_inputs/SA_vaxHest_fitted_results.Rdata')
+    loaded_fit = SA_vaxHest_fitted_results
+       
+    parameters = loaded_fit[[1]]
+    fitted_next_state = loaded_fit[[2]]
+    fitted_incidence_log_tidy = loaded_fit[[3]]
+    fitted_incidence_log = loaded_fit[[4]]
+    fitted_incidence_log_tidy = fitted_incidence_log_tidy %>% filter(date <= date_start) # CHECKED last of fitted log = first of new log
+    fitted_incidence_log = fitted_incidence_log %>% filter(date <= date_start)
   }
 }
 if ( debug == "on" | fitting == "on"){
@@ -169,7 +186,7 @@ if (prev_discounting_rate != discounting_rate){stop('need to re-run "(mech shop)
 
 #making some interim variables to assist with configuring states
 disease_class_list = c('S','E','I','R')
-num_vax_doses = D = length(unique(vaccination_history_TRUE$dose))  # dose 1, dose 2, COMEBACK no boosters yet in these settings 
+num_vax_doses = D = length(unique(vaccination_history_TRUE$dose))  # dose 1, dose 2, COMEBACK no boosters yet in these settings
 vax_type_list = sort(unique(vaccination_history_TRUE$vaccine_type))
 num_vax_types = T = length(unique(vaccination_history_TRUE$vaccine_type))
 num_vax_classes = num_vax_doses*num_vax_types + 1                 # + 1 for unvaccinated
