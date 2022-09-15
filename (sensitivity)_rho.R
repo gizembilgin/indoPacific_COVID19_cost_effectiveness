148466a738a1c3703df1449991b6e94d186d825d
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2022-09-16 08:51:24 +1000

    empty or unused

--- (sensitivity)_rho.R
+++
@@ -1,5 +0,0 @@
-#Run sensitivity analysis using: 
-    # waning_toggle_rho_acqusition = TRUE/FALSE
-    # lengthInfectionDerivedImmunity = 180,365,730
-    # rho_severe_disease = "on"/"off"
-    # hybrid immunity estimates? e.g., Cerqueira et al.
--- FleetDeputy.R
+++
@@ -1,6 +0,0 @@
-#This file will control all sensitivity analysis
-
-#(A) different time since outbreak
-#check impact on booster dose q
-
-#(sensitivity rho)
