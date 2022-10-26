92a82828fe8e7b98531c4d39b72a008b42aae8cd
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2022-10-27 08:02:36 +1100

    assume older adults lower VE

--- (antiviral) exploring results.R
+++ (antiviral) exploring results.R
@@ -1,12 +1,12 @@
 workshop = RECORD_antiviral_model_simulations 
 workshop$outcome = workshop$outcome = factor(workshop$outcome,levels=c('severe_disease','hosp','death','YLL','neonatal_deaths'))
 workshop = workshop%>% 
-  filter(VE_sensitivity_analysis == "off" &
+  filter(
            antiviral_type == "paxlovid" &
            result %in% c('average','percentage', "antiviral_rollout_capacity_utilised","antiviral_eligible_pop_coverage" )) %>%
   arrange(outcome) %>%
   mutate(value = round(as.numeric(value),digits=1)) %>%
-  select(-VE_sensitivity_analysis, -antiviral_type, -vax_scenario_risk_group) %>% 
+  select(-antiviral_type, -vax_scenario_risk_group) %>% 
   pivot_wider(
     id_cols = c(antiviral_target,vax_scenario,result),
     names_from = outcome,
