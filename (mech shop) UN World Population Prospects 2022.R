b6ddd15b0743ca2efc8807e4658d96f6852a0eab
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2022-11-24 09:00:54 +1100

    creating UN pop est

--- (mech shop) UN World Population Prospects 2022.R
+++ (mech shop) UN World Population Prospects 2022.R
@@ -1,15 +1,19 @@
 
+estimates_YEAR = 2022
+
 ### (1/3) Population by age (single years)
 workshop <- read.csv(paste(rootpath,"inputs/UN_world_population_prospects/WPP2022_PopulationBySingleAgeSex_Medium_1950-2021.csv",sep=''), header = TRUE)
 workshop2 <- read.csv(paste(rootpath,"inputs/UN_world_population_prospects/WPP2022_PopulationBySingleAgeSex_Medium_2022-2100.csv",sep=''), header = TRUE)
 workshop = rbind(workshop,workshop2); rm(workshop2)
 workshop = workshop %>%
   filter(LocTypeName == "Country/Area") %>% 
-  filter(Time >= 2021 & Time<2025) %>%
+  filter(Time == estimates_YEAR) %>%
   select(ISO3_code,Location,Time,AgeGrp,PopFemale,PopTotal) %>%
   mutate(PopFemale = PopFemale * 1000,
          PopTotal = PopTotal * 1000)
-UN_pop_est = workshop
+UN_pop_est = workshop %>%
+  mutate(AgeGrp = case_when(AgeGrp == "100+" ~ "100", TRUE ~ AgeGrp)) %>%
+  mutate(AgeGrp = as.numeric(AgeGrp))
 save(UN_pop_est,file = "1_inputs/UN_world_population_prospects/UN_pop_est.Rdata")
 
 
@@ -19,10 +23,11 @@ workshop2 <- read.csv(paste(rootpath,"inputs/UN_world_population_prospects/WPP20
 workshop = rbind(workshop,workshop2); rm(workshop2)
 workshop = workshop %>%
   filter(LocTypeName == "Country/Area") %>% 
-  filter(Time >= 2021 & Time<2025) %>%
+  filter(Time == estimates_YEAR) %>%
   select(ISO3_code,Location,Time,AgeGrpStart,ex) %>%
   rename(AgeGrp = AgeGrpStart)
-UN_lifeExpect_est = workshop
+UN_lifeExpect_est = workshop %>%
+  mutate(AgeGrp = as.numeric(AgeGrp))
 save(UN_lifeExpect_est,file = "1_inputs/UN_world_population_prospects/UN_lifeExpect_est.Rdata")
 
 
@@ -30,9 +35,10 @@ save(UN_lifeExpect_est,file = "1_inputs/UN_world_population_prospects/UN_lifeExp
 workshop <- read.csv(paste(rootpath,"inputs/UN_world_population_prospects/WPP2022_Fertility_by_Age1.csv",sep=''), header = TRUE)
 workshop = workshop %>%
   filter(LocTypeName == "Country/Area") %>% 
-  filter(Time >= 2021 & Time<2025) %>%
+  filter(Time == estimates_YEAR) %>%
   filter(Variant ==  "Medium") %>%
   select(ISO3_code, Location, Time, AgeGrp, ASFR) %>%
   mutate(ASFR = ASFR / 1000)
-UN_ASFR_est = workshop
+UN_ASFR_est = workshop %>%
+  mutate(AgeGrp = as.numeric(AgeGrp))
 save(UN_ASFR_est,file = "1_inputs/UN_world_population_prospects/UN_ASFR_est.Rdata")
