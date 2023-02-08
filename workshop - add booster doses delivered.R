b1710201625fedad071d6c1f1f82b56085063ec7
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-02-08 14:25:20 +1100

    Update workshop - add booster doses delivered.R

--- workshop - add booster doses delivered.R
+++ workshop - add booster doses delivered.R
@@ -1,3 +1,11 @@
+setting = "PNG"
+
+risk_group_name = "pregnant_women" #options: pregnant_women, adults_with_comorbidities
+
+source(paste(getwd(),"/(1)_simulate_setting.R",sep="")) #load setting stats if new setting
+
+###go back and run queue to set up commands
+
 booster_dose_delivered_log = data.frame()
 
 for (ticket in 1:length(queue)){
@@ -21,36 +29,11 @@ for (ticket in 1:length(queue)){
   ### sensitivity analysis - booster doses in 2023
   if (exists("booster_toggles") == FALSE){booster_toggles = "no"}
   if (length(booster_toggles)>1){
-    
-    if (booster_toggles$function_name == "booster_prioritised_strategies"){
-      source(paste(getwd(),"/(function)_booster_dose_delivery.R",sep=""))
-      source(paste(getwd(),"/(function)_prioritised_booster_dose_delivery.R",sep=""))
-      
-      vaccination_history_FINAL =
-        booster_strategy_prioritised(
-          booster_risk_strategy = booster_prioritised_strategies$strategy,
-          booster_risk_proportion = booster_prioritised_strategies$risk_proportion
-        )
-      rm(booster_strategy_prioritised)
-      
-    } else if (booster_toggles$function_name == "booster_strategy"){
-      source(paste(getwd(),"/(function)_booster_dose_delivery.R",sep=""))
-      vaccination_history_FINAL =
-        booster_strategy( booster_strategy_start_date = booster_toggles$start_date,       # start of hypothetical vaccination program
-                          booster_dose_allocation     = booster_toggles$dose_allocation,  # num of doses avaliable
-                          booster_rollout_speed       = booster_toggles$rollout_speed,    # doses delivered per day
-                          booster_delivery_risk_group = booster_toggles$delivery_risk_group,
-                          booster_delivery_includes_previously_boosted = booster_toggles$delivery_includes_previously_boosted,
-                          booster_age_strategy        = booster_toggles$age_strategy,     # options: "oldest", "youngest","50_down","uniform"
-                          booster_strategy_vaccine_type = booster_toggles$vaccine_type,   # options: "Moderna","Pfizer","AstraZeneca","Johnson & Johnson","Sinopharm","Sinovac"
-                          booster_strategy_vaccine_interval = booster_toggles$vaccine_interval
-        )
-      rm(booster_strategy)
-    } else if (booster_toggles$function_name == "booster_strategy_informed_prior"){
+    if (booster_toggles$function_name == "booster_strategy_informed_prior"){
       source(paste(getwd(),"/(function)_booster_strategy_informed_prior.R",sep=""))
       workshop = booster_strategy_informed_prior(booster_strategy_start_date = booster_toggles$start_date,       # start of hypothetical vaccination program
                                                  booster_dose_supply        = booster_toggles$dose_supply,      # num of doses avaliable
-                                                 booster_rollout_months     = booster_toggles$rollout_months,   # number of months to complete booster program
+                                                 #booster_rollout_months     = booster_toggles$rollout_months,   # number of months to complete booster program
                                                  
                                                  booster_delivery_risk_group = booster_toggles$delivery_risk_group,
                                                  booster_prev_dose_floor     = booster_toggles$prev_dose_floor, #down to what dose is willing to be vaccinated?
@@ -114,10 +97,11 @@ for (ticket in 1:length(queue)){
   
   row = data.frame(outcome = "booster_doses_delivered",
                            overall = sum(additional_booster_doses$doses),
-                           high_risk = additional_booster_doses$doses[additional_booster_doses$risk_group == risk_group_name],
+                           high_risk = sum(additional_booster_doses$doses[additional_booster_doses$risk_group == risk_group_name]),
                            vax_scenario = vax_strategy_description,
                            vax_scenario_risk_group = risk_group_name)
   booster_dose_delivered_log = rbind(booster_dose_delivered_log,row)
   
   
 }
+
