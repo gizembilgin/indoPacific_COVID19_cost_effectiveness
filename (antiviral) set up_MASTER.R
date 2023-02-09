ddcd6a9f2ab942460df571afcca41a8aa938a6b2
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-02-09 13:23:28 +1100

    Update (antiviral) set up_MASTER.R

--- (antiviral) set up_MASTER.R
+++ (antiviral) set up_MASTER.R
@@ -1,11 +1,11 @@
 rm(list=ls())
-master_toggles = list(setting_beta = "PNG_low_beta",
+master_toggles = list(setting_beta = "FJI",
                       risk_group_name = "adults_with_comorbidities")
 source(paste(getwd(),"/(antiviral) set up.R",sep=""),local=TRUE)
 source(paste(getwd(),"/(antiviral)(run).R",sep=""),local=TRUE)
 
 rm(list=ls())
-master_toggles = list(setting_beta = "PNG_low_beta",
+master_toggles = list(setting_beta = "FJI",
                       risk_group_name = "pregnant_women")
 source(paste(getwd(),"/(antiviral) set up.R",sep=""),local=TRUE)
-source(paste(getwd(),"/(antiviral)(run) special case pregnant women.R",sep=""),local=TRUE)
+source(paste(getwd(),"/(antiviral)(run) special case pregnant women .R",sep=""),local=TRUE)
