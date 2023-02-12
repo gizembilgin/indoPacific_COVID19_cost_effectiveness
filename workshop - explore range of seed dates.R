6fada5f1d185411e1136a7efcaa2a56bebae318d
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-02-12 12:14:41 +1100

    Update workshop - explore range of seed dates.R

--- workshop - explore range of seed dates.R
+++ workshop - explore range of seed dates.R
@@ -563,10 +563,10 @@ save(TLS_tracker, file = paste('1_inputs/fit/TLS_tracker.Rdata',sep=''))
 
 
 ### WAVE ONE - IDN
-train_inital = strain_now = 'WT' 
+strain_inital = strain_now = 'WT' 
 
 fitting_beta= rep(1,length(reported_peaks))
-under_reporting_est = 100
+under_reporting_est = 150
 
 baseline_covid19_waves = data.frame(date = c(as.Date('2020-04-01'),as.Date('2021-04-01'),as.Date('2021-12-01')),
                                     strain = c('WT','delta','omicron'))
@@ -575,9 +575,9 @@ plot_tracker = data.frame()
 fit_tracker = data.frame()
 workshop_incidence_log_tracker = data.frame()
 
-for (this_beta_mod in c(0.8,0.9,1,1.1)) {
-  for (this_shift in c(0,30,60,90,105,120,135,150,180,240)) {
-    if(nrow(plot_tracker[plot_tracker$shift == this_shift & plot_tracker$beta_mod == this_beta,])>0){
+for (this_beta_mod in c(1,1.05,1.1)) {
+  for (this_shift in c(135,142,150,157,165)) {
+    if(nrow(plot_tracker[plot_tracker$shift == this_shift & plot_tracker$beta_mod == this_beta_mod,])>0){
       #skip
     } else{
       strain_inital = strain_now = 'WT' 
@@ -586,23 +586,20 @@ for (this_beta_mod in c(0.8,0.9,1,1.1)) {
       covid19_waves = baseline_covid19_waves 
       covid19_waves$date[1] = covid19_waves$date[1] + this_shift
       date_start = covid19_waves$date[1] - 7
-      model_weeks = as.numeric(((sort(reported_peaks)[2]-sort(reported_peaks)[1])/2+sort(reported_peaks)[1]-date_start)/7)
+      model_weeks = as.numeric((fit_cutoff_dates[1]-date_start)/7)
       
       source(paste(getwd(),"/CommandDeck.R",sep=""))
       
       workshop = case_history %>%
         select(date,rolling_average) %>%
         mutate(#under_reporting_est = coeff1 + coeff2*as.numeric(date - date_start), #linear
-          rolling_average = case_when(
-            date > fit_cutoff_dates[2] ~ rolling_average * under_reporting_est,
-            date > fit_cutoff_dates[1] ~ rolling_average * second_wave_fit$par[2],
-            date <= fit_cutoff_dates[1] ~ rolling_average * first_wave_fit$par[2])) %>%
+          rolling_average =  rolling_average * under_reporting_est) %>%
         rename(adjusted_reported = rolling_average) %>%
         left_join(incidence_log, by = "date") %>%
         mutate(fit_statistic = abs(rolling_average - adjusted_reported)^2 ,
                beta_mod = this_beta_mod, shift = this_shift)
       
-      fit_statistic = data.frame(fit = sum(workshop$fit_statistic[workshop$date> fit_cutoff_dates[2]], #fit only after first wave
+      fit_statistic = data.frame(fit = sum(workshop$fit_statistic, #fit only after first wave
                                            na.rm=TRUE),
                                  beta_mod = this_beta_mod, shift = this_shift)
       
@@ -619,11 +616,11 @@ for (this_beta_mod in c(0.8,0.9,1,1.1)) {
 beta_fit_tracker = fit_tracker; beta_fit_tracker
 beta_tracker = plot_tracker %>%
   filter(date<= max(plot_tracker$date[is.na(plot_tracker$rolling_average) == FALSE]) & 
-           date>= min(plot_tracker$date[is.na(plot_tracker$rolling_average) == FALSE])) #%>%
-  #filter(beta_mod %in% c(1, 1.1,1.2,1.35,1.5) & shift %in% c(0,15,30,45))
+           date>= min(plot_tracker$date[is.na(plot_tracker$rolling_average) == FALSE])) %>%
+  filter(!(shift %in% c(120,180,195)) & beta_mod != 0.9)
 ggplot() +
   geom_line(data=beta_tracker,aes(x=date,y=rolling_average,color=as.factor(beta_mod)),na.rm=TRUE) +
-  geom_point(data=case_history[case_history$date<=max(beta_tracker$date),],aes(x=date,y=rolling_average*100)) +
+  geom_point(data=case_history[case_history$date<=max(beta_tracker$date),],aes(x=date,y=rolling_average*150)) +
   plot_standard + 
   labs(color = 'beta modifier')+ 
   facet_grid(shift ~ .) 
