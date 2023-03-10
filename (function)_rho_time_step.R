fb3b9835326e64e3606a503b3024df3f33cc05c9
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-03-10 17:41:06 +1100

    omicron truncation factor

--- (function)_rho_time_step.R
+++ (function)_rho_time_step.R
@@ -59,7 +59,12 @@ for (i in 1:length(unique(intro_raw$strain))){
   this_raw = this_raw %>% 
     mutate(days = as.numeric(date - min(this_raw$date)),
            percentage = percentage/100)
-  if(exists("TOGGLE_delta_truncation_factor")){this_raw$days = this_raw$days * TOGGLE_delta_truncation_factor}
+  if(exists("TOGGLE_delta_truncation_factor") & this_strain == "delta"){
+    this_raw$days = this_raw$days * TOGGLE_delta_truncation_factor
+  }
+  if(exists("TOGGLE_omicron_truncation_factor") & this_strain == "omicron"){
+    this_raw$days = this_raw$days * TOGGLE_omicron_truncation_factor
+  }
   
   ggplot(this_raw) + geom_point(aes(x=days,y=percentage))
   
