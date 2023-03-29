32ae8f3040af69e1b452d7f746ede3c31500bf52
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-03-30 09:29:01 +1100

    update rho SO

--- (function)_rho_time_step.R
+++ (function)_rho_time_step.R
@@ -8,32 +8,70 @@
 
 
 ### (1) Shape of waning ###############################################################################
-#Exponential fit separately, see Supplementary Material for data source
-rho_dn = crossing(type = c('new_to_new','prev_to_new'),
-                  outcome = c('symptomatic_disease','severe_outcome'),
-                  days = seq(0,365*2)
-)
-rho_dn = rho_dn %>% mutate(
-  protection = case_when(
-    outcome == 'symptomatic_disease' & type == 'prev_to_new' ~ 0.9932*exp(-0.004*days),
-    outcome == 'symptomatic_disease' & type == 'new_to_new' ~ 0.9654*exp(-0.0002*days),
-    outcome == 'severe_outcome' ~ 0.88 
-  )
-)
+##### rho against infection
+#See Supplementary Material for data source
+rho_dn = crossing(type =  c('new_to_new','prev_to_new'),
+                  outcome = c('symptomatic_disease'),
+                  days = seq(0,365*2))
+                  
+                  
+rho_dn = rho_dn %>% mutate( protection = case_when(
+    type == 'prev_to_new' ~ 0.9932 * exp(-0.004 * days),
+    type == 'new_to_new' ~ 0.9654 * exp(-0.0002 * days)
+  ))
 
-rho_est_plot = ggplot() + 
-  geom_line(data = rho_dn[rho_dn$outcome == 'symptomatic_disease',],aes(days,protection,color=as.factor(type))) + 
-  ylim(0,1) +labs(title = 'symptomatic_disease') +
+rho_dn_wide = rho_dn %>% pivot_wider(
+  id_cols = days,
+  names_from = type,
+  values_from = protection
+)
+#____________________________________________________
+
+
+### rho against severe outcome
+# Niklas Bobrovitz, Harriet Ware, Xiaomeng Ma, Zihan Li, Reza Hosseini, Christian Cao, Anabel Selemon, Mairead Whelan, Zahra Premji, Hanane Issa, Brianna Cheng, Laith J Abu Raddad, David L Buckeridge, Maria D Van Kerkhove, Vanessa Piechotta, Melissa M Higdon, Annelies Wilder-Smith, Isabel Bergeri, Daniel R Feikin, Rahul K Arora, Minal K Patel, Lorenzo Subissi,
+# Protective effectiveness of previous SARS-CoV-2 infection and hybrid immunity against the omicron variant and severe disease: a systematic review and meta-regression,
+# The Lancet Infectious Diseases,2023,
+# ISSN 1473-3099,
+# https://doi.org/10.1016/S1473-3099(22)00801-5.
+hybrid_raw <- read.csv("1_inputs/hybrid_immunity.csv")
+hybrid_raw = hybrid_raw %>% 
+  filter(immunity_type == "previous_infection" &
+           protection_against == "severe_disease") %>%
+  mutate(day = (365/12) * month) %>%
+  filter(is.na(rho) == FALSE)
+ggplot() + 
+  geom_pointrange(data=hybrid_raw,
+                  aes(y=rho*100,x=month,
+                      ymin=LB*100,ymax=UB*100
+                  )) +
+  labs(color="") +
+  xlab('months since recovery') +
+  ylab("protection (%)") +
   theme_bw() +
-  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())   
+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank()) 
 
-rho_dn_wide = rho_dn %>% 
-  filter(outcome == 'symptomatic_disease') %>%
-  pivot_wider(
-    id_cols = days,
-    names_from = type,
-    values_from = protection
-  )
+days_list = c(ceiling(min(hybrid_raw$day)):ceiling(max(hybrid_raw$day)))
+smooth <- loess(rho ~ day,  data=hybrid_raw)
+smooth <- predict(smooth,days_list )
+
+smooth = data.frame(cbind(days_list,smooth)) 
+colnames(smooth) = c('day','rho_est')
+
+filler_start = crossing(day = c(0:(min(smooth$day)-1)),
+                        rho_est = max(smooth$rho_est,na.rm=TRUE))
+filler_end = crossing(day = c((max(smooth$day)+1):730),
+                      rho_est = min(smooth$rho_est,na.rm=TRUE))
+
+workshop = rbind(filler_start,smooth,filler_end) %>% 
+  filter(is.na(rho_est) == FALSE) %>%
+  mutate(outcome = "severe_outcome") %>%
+  rename(days = day) %>%
+  rename(protection = rho_est)
+
+ggplot(workshop) + geom_line(aes(x=days,y=protection))
+
+rho_dn = bind_rows(rho_dn,workshop) 
 #######################################################################################################
 
 
@@ -129,10 +167,9 @@ rm(intro_raw,this_strain,this_raw,df_this_strain,days_to_predict,smoothed_spline
 
 
 ### (3) Tracking of days since infection (proxy) for people in Recovered class ########################
-rho_acq = rho_dn %>% filter(outcome == 'symptomatic_disease')
-
 # This function calculates the population-level infection-derived protection against reinfection at any given time by age_group
-rho_time_step <- function(date_now){
+rho_time_step <- function(date_now,
+                          outcome = "acquisition"){
   
   #create data set of historical cases by day in recovery class
   if (exists("incidence_log") == FALSE){ incidence_log = data.frame() }
@@ -159,61 +196,68 @@ rho_time_step <- function(date_now){
   ggplot(workshop) + geom_line(aes(date,prop_window)) 
   #______________________
   
-  
-  #calculate protection to latest versus previous strains
-  if (nrow(covid19_waves[covid19_waves$date<date_now & covid19_waves$strain == "omicron",])>0){ # do we have any immune escape variants?
-    
-    num_variants_introduced = nrow(covid19_waves[covid19_waves$date<date_now & covid19_waves$strain == "omicron",])
-    
-    #rho = how protected are people today from the circulating strains today?
-    #    = %new circulating today * protection to new today + %old circulating today * protection to old today
-    
-    for (variant_num in 1:num_variants_introduced){
+  if (outcome == "acquisition"){
+    #calculate protection to latest versus previous strains
+    if (nrow(covid19_waves[covid19_waves$date<date_now & covid19_waves$strain == "omicron",])>0){ # do we have any immune escape variants?
       
-      this_variant_introduction = covid19_waves$date[covid19_waves$strain == "omicron"][variant_num]
+      num_variants_introduced = nrow(covid19_waves[covid19_waves$date<date_now & covid19_waves$strain == "omicron",])
       
-      this_variant_shift = synthetic_strain_shift %>% filter(strain == covid19_waves$strain[covid19_waves$date == this_variant_introduction]) %>% 
-        select(days,percentage) %>%
-        mutate(date = days + this_variant_introduction) %>%
-        select(-days)
-      this_variant_shift$date = as.Date(this_variant_shift$date, format =  '%Y-%m-%d')
+      #rho = how protected are people today from the circulating strains today?
+      #    = %new circulating today * protection to new today + %old circulating today * protection to old today
       
-      if (variant_num == 1){
-        protection_to_old = workshop %>% 
-          left_join(rho_acq[rho_acq$type == 'new_to_new',], by = "days") %>% 
-          mutate(interim = protection * prop_window)
-        protection_to_old = sum(protection_to_old$interim,na.rm=TRUE)
-      } else{
-        protection_to_old = protection
+      for (variant_num in 1:num_variants_introduced){
+        
+        this_variant_introduction = covid19_waves$date[covid19_waves$strain == "omicron"][variant_num]
+        
+        this_variant_shift = synthetic_strain_shift %>% filter(strain == covid19_waves$strain[covid19_waves$date == this_variant_introduction]) %>% 
+          select(days,percentage) %>%
+          mutate(date = days + this_variant_introduction) %>%
+          select(-days)
+        this_variant_shift$date = as.Date(this_variant_shift$date, format =  '%Y-%m-%d')
+        
+        if (variant_num == 1){
+          protection_to_old = workshop %>% 
+            left_join(rho_dn[rho_dn$type == 'new_to_new' & rho_dn$outcome == 'symptomatic_disease',], by = "days") %>% 
+            mutate(interim = protection * prop_window)
+          protection_to_old = sum(protection_to_old$interim,na.rm=TRUE)
+        } else{
+          protection_to_old = protection
+        }
+        
+        protection_to_new = workshop %>%
+          left_join(rho_dn_wide, by = "days") %>% 
+          left_join(this_variant_shift,by="date") %>%
+          mutate(interim = case_when(
+            date < this_variant_introduction ~ prev_to_new * prop_window,
+            date >= this_variant_introduction & date <= max(this_variant_shift$date)~(prev_to_new*(1-percentage)+new_to_new*percentage)*prop_window,
+            date > max(this_variant_shift$date) ~ new_to_new*prop_window
+          ))
+        protection_to_new = sum(protection_to_new$interim,na.rm=TRUE)
+        
+        if (date_now %in% this_variant_shift$date){
+          percentage_new = this_variant_shift$percentage[this_variant_shift$date == date_now]
+        } else{
+          percentage_new = this_variant_shift$percentage[this_variant_shift$date == max(this_variant_shift$date)]
+        }
+        
+        protection = percentage_new * protection_to_new + (1-percentage_new) * protection_to_old
       }
       
-      protection_to_new = workshop %>%
-        left_join(rho_dn_wide, by = "days") %>% 
-        left_join(this_variant_shift,by="date") %>%
-        mutate(interim = case_when(
-          date < this_variant_introduction ~ prev_to_new * prop_window,
-          date >= this_variant_introduction & date <= max(this_variant_shift$date)~(prev_to_new*(1-percentage)+new_to_new*percentage)*prop_window,
-          date > max(this_variant_shift$date) ~ new_to_new*prop_window
-        ))
-      protection_to_new = sum(protection_to_new$interim,na.rm=TRUE)
+      function_result = protection
       
-      if (date_now %in% this_variant_shift$date){
-        percentage_new = this_variant_shift$percentage[this_variant_shift$date == date_now]
-      } else{
-        percentage_new = this_variant_shift$percentage[this_variant_shift$date == max(this_variant_shift$date)]
-      }
+    } else{
+      workshop = workshop %>% 
+        left_join(rho_dn[rho_dn$type == 'new_to_new' & rho_dn$outcome == 'symptomatic_disease',], by = "days") %>%
+        mutate(interim = protection * prop_window)
       
-      protection = percentage_new * protection_to_new + (1-percentage_new) * protection_to_old
+      function_result = sum(workshop$interim,na.rm=TRUE) 
     }
-    
-    function_result = protection
-    
-  } else{
-    workshop = workshop %>% 
-      left_join(rho_acq[rho_acq$type == 'new_to_new',], by = "days") %>%
-      mutate(interim = protection * prop_window)
-    
-    function_result = sum(workshop$interim,na.rm=TRUE) 
+  } else  if (outcome == "severe_disease"){
+    #LIMITATION - not by age group, but variation minimal between 0.72-0.82
+    function_result = workshop %>% 
+      left_join(rho_dn[rho_dn$outcome == 'severe_outcome',], by = "days") %>%
+      mutate(interim = protection * prop_window) %>%
+      summarise(protection = sum(interim,na.rm=TRUE),.groups="keep") 
   }
   
   return(function_result)
