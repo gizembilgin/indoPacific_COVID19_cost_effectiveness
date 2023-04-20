968f24efba2fb914a027f8cb4c8adf6f40cde3ed
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-04-21 08:32:37 +1000

    Create CommandDeck.R

---
+++ 4_cost_effectiveness_analysis/CommandDeck.R
@@ -0,0 +1,15 @@
+
+require(readr); require(ggplot2); require(tidyverse)
+
+this_setting = "FJI"
+
+TOGGLE_uncertainty = "fixed"
+TOGGLE_numberOfRuns = 1
+if (TOGGLE_uncertainty == "fixed"){TOGGLE_numberOfRuns = 1}
+
+
+
+for (ticket in 1:TOGGLE_numberOfRuns){
+  
+}
+
