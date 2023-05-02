fd59bf078abda5403596d847a967a3627c68f3f6
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-05-02 15:48:21 +1000

    CEA CommandDeck reflecting new script names

--- 4_cost_effectiveness_analysis/CommandDeck.R
+++ 4_cost_effectiveness_analysis/CommandDeck.R
@@ -1,7 +1,7 @@
 
 require(readr); require(ggplot2); require(tidyverse)
-source(paste(getwd(),"/(function)_Translator.R",sep=""),local=TRUE)
-source(paste(getwd(),"/(function)_QALY_estimator.R",sep=""),local=TRUE)
+
+source(paste(getwd(),"/(function)_outcomesAverted_estimator.R",sep=""),local=TRUE)
 source(paste(getwd(),"/(function)_interventionCost_estimator.R",sep=""),local=TRUE)
 
 LIST_CEA_settings = list("TLS")
@@ -20,23 +20,14 @@ for (ticket in 1:TOGGLE_numberOfRuns){
   ###(1/3) Load antiviral model runs
   
   ###(2/3) Calculate QALYs, intervention costs, and healthcare costs averted
+    
   outcomes_averted <- outcomesAverted_estimator(LIST_CEA_settings,
                                                 toggle_longCOVID = TOGGLE_longCOVID,
                                                  toggle_discounting_rate = TOGGLE_discounting_rate)
-  #NB: No uncertainty in these estimates as data sources are expert point estimates of:
-  #       population (UN), life expectancy (UN), HRQoL (Robinson, Eber & Hammitt), and age_severity_specific_QALYs (Robinson, Eber & Hammitt)
-  
-  
+    
   interventionCost_estimates <- interventionCost_estimator(LIST_CEA_settings,
                              antiviral_cost_scenario = "low_generic_cost",
                              wastage_rate_antiviralSchedule = 0)
-  
-  
-  #rnorm(count = number of outcome, mean = est, sd = )
-  
-  
-
-  
   
 
   
