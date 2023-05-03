99fea195a35e9689faa9602522a87a5fbe2ba726
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-05-04 09:46:42 +1000

    Update CommandDeck.R

--- 4_cost_effectiveness_analysis/CommandDeck.R
+++ 4_cost_effectiveness_analysis/CommandDeck.R
@@ -3,14 +3,18 @@ require(readr); require(ggplot2); require(tidyverse)
 
 source(paste(getwd(),"/(function)_outcomesAverted_estimator.R",sep=""),local=TRUE)
 source(paste(getwd(),"/(function)_interventionCost_estimator.R",sep=""),local=TRUE)
+source(paste(getwd(),"/(function)_healthCareCostsAverted_estimator.R",sep=""),local=TRUE)
+source(paste(getwd(),"/(function)_CEA_calculation.R",sep=""),local=TRUE)
 
 LIST_CEA_settings = list("TLS")
 CEA_risk_group = "adults_with_comorbidities"
 
-TOGGLE_uncertainty = "fixed"
+TOGGLE_uncertainty = "fixed" #fixed or rand
 TOGGLE_numberOfRuns = 1
 TOGGLE_discounting_rate = 0.03
 TOGGLE_longCOVID = "off"
+TOGGLE_antiviral_cost_scanario = "low_generic_cost"# options: low_generic_cost,middle_income_cost, high_income_cost
+
 if (TOGGLE_uncertainty == "fixed"){TOGGLE_numberOfRuns = 1}
 
 
@@ -28,12 +32,20 @@ for (ticket in 1:TOGGLE_numberOfRuns){
   #               outcomes_averted by setting,outcome,booster_vax_scenario,intervention,intervention_target_group
     
   interventionCost_estimates <- interventionCost_estimator(LIST_CEA_settings,
-                             antiviral_cost_scenario = "low_generic_cost",
+                             antiviral_cost_scenario = TOGGLE_antiviral_cost_scanario,
                              wastage_rate_antiviralSchedule = 0)
+  
+  healthcareCostEstimation <- healthCareCostsAverted_estimator(LIST_CEA_settings,
+                                                               toggle_uncertainty = TOGGLE_uncertainty,
+                                                               TORNADO_PLOT_OVERRIDE = list())
   
 
   
   ###(3/3) CEA per setting
+  result <- CEA_calculation(outcomesAvertedEstimation,
+                            interventionCost_estimates,
+                            healthcareCostEstimation)
+  
   #include plot of QALY breakdown by outcome_source
   
   
