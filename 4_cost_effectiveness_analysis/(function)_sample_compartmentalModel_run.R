6c9e5ae15576f1a469aa85757c73d9c280c359f7
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-05-29 12:28:27 +1000

    subset MASTER_antiviral_simulations
    subset MASTER_antiviral_simulations to desired vax and antiviral eligb scenarios
    + allow toggle_uncertainty == "fixed

--- 4_cost_effectiveness_analysis/(function)_sample_compartmentalModel_run.R
+++ 4_cost_effectiveness_analysis/(function)_sample_compartmentalModel_run.R
@@ -1,6 +1,10 @@
 
 sample_compartmentalModel_run <- function(LIST_CEA_settings,
-                                          sampling_strategy = "empirical_distribution"){
+                                          LIST_booster_vax_scenarios,
+                                          LIST_antiviral_elig_groups,
+                                          LIST_antiviral_types,
+                                          sampling_strategy = "empirical_distribution",
+                                          toggle_uncertainty = TOGGLE_uncertainty){
   
   rootpath = str_replace(getwd(), "GitHub_vaxAllocation/4_cost_effectiveness_analysis","")
   MASTER_antiviral_simulations = data.frame()
@@ -33,7 +37,14 @@ sample_compartmentalModel_run <- function(LIST_CEA_settings,
   rm(RECORD_antiviral_model_simulations)
   
   #CHOICE - sampling from the empirical distribution of each parameter created by 100 simulations; OR take one stochasticmodel run 
-  if (sampling_strategy == "empirical_distribution"){
+  if (toggle_uncertainty == "fixed"){
+    sampled_df = MASTER_antiviral_simulations %>%
+      group_by(outcome, antiviral_type, antiviral_target_group, intervention, evaluation_group, vax_scenario, vax_scenario_risk_group, age_group, result,country,setting_beta,setting) %>%
+      summarise(intervention_doses_delivered = mean(intervention_doses_delivered),
+                value = mean(value),
+                .groups="keep") %>%
+      ungroup()
+  } else if (sampling_strategy == "empirical_distribution"){
     sampled_df = MASTER_antiviral_simulations %>%
       group_by(outcome, antiviral_type, antiviral_target_group, intervention, evaluation_group, vax_scenario, vax_scenario_risk_group, age_group, result,country,setting_beta,setting) %>%
       summarise(intervention_doses_delivered = sample(intervention_doses_delivered, size = 1),
@@ -49,6 +60,10 @@ sample_compartmentalModel_run <- function(LIST_CEA_settings,
   }
   if (nrow(sampled_df) != nrow(MASTER_antiviral_simulations)/100){stop("Did you not run 100 simulations?")}
 
+  sampled_df = sampled_df %>%
+    filter(vax_scenario %in% LIST_booster_vax_scenarios) %>%
+    filter(antiviral_type %in% LIST_antiviral_types | is.na(antiviral_type)) %>%
+    filter(antiviral_target_group %in% LIST_antiviral_elig_groups | is.na(antiviral_target_group))
 
   return(sampled_df)
 }
