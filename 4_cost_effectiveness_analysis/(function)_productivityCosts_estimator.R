3613b91d8911d8fe73b4a465d593f5a97417fe7f
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-06-30 09:53:06 +1000

    including productivity loss due to illness
    also removing calculation of discounting in this f(x) to save time;
    instead, pre-saving discounting for 0-10% by 1% increments in the mech shop

--- 4_cost_effectiveness_analysis/(function)_productivityCosts_estimator.R
+++ 4_cost_effectiveness_analysis/(function)_productivityCosts_estimator.R
@@ -9,24 +9,17 @@ productivityCosts_estimator <- function(
     this_risk_group = "adults_with_comorbidities"
 ){
   
+  if (!(toggle_discounting_rate %in% seq(0,0.1,by = 0.01))){
+    stop('Go back to (mech shop) productivity loss and ensure that productivity losses by this discounting rate are calculated')
+  }
+  
   # Step One: load inputs
   setting_list = LIST_CEA_settings
-  age_groups_num = c(0,4,9,17,29,44,59,69,110)
-  age_group_labels = c('0 to 4','5 to 9','10 to 17','18 to 29','30 to 44','45 to 59','60 to 69','70 to 100')
-  load(file = "2_inputs/expected_daily_earnings.Rdata")
-  load(file = paste(gsub("4_cost_effectiveness_analysis","",getwd()),
-                    "1_inputs/UN_world_population_prospects/UN_pop_est.Rdata",sep=''))
-  
-  pop_orig <- UN_pop_est %>% 
-    rename(population = PopTotal,
-           age = AgeGrp,
-           setting = ISO3_code) %>%
-    filter(setting %in% setting_list) %>%
-    select(setting,age,population)
-  rm(UN_pop_est)
-  
-  expected_daily_earnings = expected_daily_earnings %>%
-    rename(setting = ISO3_code)
+  load(file = "2_inputs/productivity_loss_reference_df.Rdata")
+  productivity_loss_df <- productivity_loss_reference_df %>%
+    filter(discounting_rate == toggle_discounting_rate) %>%
+    ungroup() %>%
+    select(-discounting_rate)
   
   TRANSLATED_antiviral_simulations = MASTER_antiviral_simulations %>%
     filter(is.na(age_group) == FALSE) %>%
@@ -61,115 +54,29 @@ productivityCosts_estimator <- function(
   #_______________________________________________________________________________
   
   
-  # Step Two: productivity losses due to illness
-  ###COMEBACK - need to sample for each individual
-  duration_symptomatic = 9.87
-  productivity_loss = TRANSLATED_antiviral_simulations %>%
-    filter(outcome %in% c("critical_disease","severe_disease")) %>%
-    left_join(expected_daily_earnings, by = c("setting","age_group")) %>%
-    mutate(daily_earning = daily_earning * value * duration_symptomatic) %>%
-    group_by(setting,booster_vax_scenario,intervention,intervention_target_group) %>%
-    summarise(cost = sum(daily_earning), .groups = "keep")%>%
-    mutate(productivity_loss_category = "illness")
+  # Step Two: apply productivity losses by outcome
+  productivity_loss_breakdown = TRANSLATED_antiviral_simulations %>%
+    filter(outcome %in% unique(productivity_loss_df$outcome)) %>%
+    left_join(productivity_loss_df, by = c("setting","age_group","outcome")) %>%
+    group_by(setting,booster_vax_scenario,intervention,intervention_target_group,outcome) %>%
+    summarise(cost = sum(productivity_loss), .groups = "keep")
+  
+  productivity_loss_breakdown = TRANSLATED_antiviral_simulations %>%
+    filter(outcome %in% unique(productivity_loss_df$outcome)) %>%
+    left_join(productivity_loss_df, by = c("setting","age_group","outcome")) %>%
+    mutate(productivity_loss = productivity_loss * value,
+           productivity_loss_category = case_when(
+             outcome == "death" ~ "death",
+             TRUE ~ "illness")) %>%
+    group_by(setting,booster_vax_scenario,intervention,intervention_target_group,productivity_loss_category) %>%
+    summarise(cost = sum(productivity_loss), .groups = "keep")
   #_______________________________________________________________________________
   
-  # Step Three:Expected remaining lifetime earnings
-  #Step One: load life expectancy at age X
-  #"The average number of remaining years of life expected by a hypothetical cohort of individuals alive at age x 
-  # who would be subject during the remaining of their lives to the mortality rates of a given period."
-  # https://population.un.org/wpp/Download/Standard/Mortality/
-  load(file = paste(gsub("4_cost_effectiveness_analysis","",getwd()),
-                    "1_inputs/UN_world_population_prospects/UN_lifeExpect_est.Rdata",sep=''))
-  
-  UN_lifeExpect_est = UN_lifeExpect_est %>%
-    rename(life_expectancy = ex,
-           age = AgeGrp,
-           setting = ISO3_code) %>%
-    mutate(age_group = cut(age,breaks = age_groups_num, include.lowest = T, labels = age_group_labels))  %>%
-    select(setting,age_group,age,life_expectancy)
-  
-  # ggplot(UN_lifeExpect_est[UN_lifeExpect_est$setting %in% setting_list,]) +
-  #   geom_point(aes(x=age,y=life_expectancy,color=as.factor(setting)),position="dodge")
-   
-  remaining_lifetime_earnings = expected_daily_earnings %>%
-    left_join(UN_lifeExpect_est, by = c("age_group","setting")) %>%
-    mutate(yearly_earning = daily_earning * 365)
-  
-  workshop = data.frame()
-  for (this_setting in unique(remaining_lifetime_earnings$setting)){ # for each setting
-    for (this_age in unique(remaining_lifetime_earnings$age)){ # for each age
-      
-      #how many years of life left at this age in this setting?
-      this_workshop = remaining_lifetime_earnings %>% 
-        filter(setting == this_setting &
-                 age == this_age)
-      
-      #create window of ages we are looking at
-      this_workshop_window = remaining_lifetime_earnings %>% 
-        filter(setting == this_setting &
-                 age > this_age &
-                 age <= (this_age + ceiling(this_workshop$life_expectancy))) 
-      
-      #apply discounting (if>0) to each subsequent year of life
-      if (toggle_discounting_rate>0){
-        this_workshop_window = this_workshop_window %>%
-          mutate(discounting_year = age - this_age,
-                 discounting_multiplier = 1/(1+toggle_discounting_rate)^discounting_year,
-                 yearly_earning = yearly_earning*discounting_multiplier) %>%
-          select(-discounting_year,-discounting_multiplier)
-        
-      }
-      
-      #integer component of years of life left, e.g., if life expectancy 68.8 years -> 68 years
-      full = this_workshop_window %>% filter(setting == this_setting &
-                                               age > this_age &
-                                               age <= (this_age + floor(this_workshop$life_expectancy)))
-      full = sum(full$yearly_earning)
-      
-      #decimal component of years of life left, e.g., if life expectancy 68.8 years -> 0.8 years
-      partial = this_workshop_window %>% filter(setting == this_setting &
-                                                  age == (this_age + ceiling(this_workshop$life_expectancy)))
-      partial = partial$yearly_earning*(this_workshop$life_expectancy - floor(this_workshop$life_expectancy))
-      if(length(partial) == 0){partial = 0}
-      
-      this_row = data.frame(age=this_age,
-                            setting = this_setting,
-                            life_expectancy = this_workshop$life_expectancy,
-                            remaining_lifetime_earnings = full+partial)
-      
-      workshop = rbind(workshop,this_row)
-    }
-  }
-  # ggplot(workshop) +
-  #   geom_line(aes(x=age,y=remaining_lifetime_earnings)) +
-  #   facet_grid(setting ~.)
-  
-  
-  # calculate remaining_lifetime_earnings per model age group weighted by pop dn
-  remaining_lifetime_earnings = workshop %>%
-    left_join(pop_orig, by = c('age','setting')) %>%
-    select(setting,age,remaining_lifetime_earnings,population) %>%
-    mutate(age_group = cut(age,breaks = age_groups_num, include.lowest = T,labels = age_group_labels)) %>%
-    group_by(setting,age_group) %>%
-    mutate(group_percent = population/sum(population),
-           interim = remaining_lifetime_earnings * group_percent) %>%
-    summarise(remaining_lifetime_earnings = sum(interim),.groups="keep") 
-  #ggplot(remaining_lifetime_earnings[remaining_lifetime_earnings$setting %in% setting_list,]) + geom_col(aes(x=age_group,y=remaining_lifetime_earnings,fill=as.factor(setting)),position="dodge")
-  
-  workshop = TRANSLATED_antiviral_simulations %>%
-    filter(outcome %in% c("death")) %>%
-    left_join(remaining_lifetime_earnings, by = c("setting","age_group")) %>%
-    mutate(remaining_lifetime_earnings = remaining_lifetime_earnings * value) %>%
-    group_by(setting,booster_vax_scenario,intervention,intervention_target_group) %>%
-    summarise(cost = sum(remaining_lifetime_earnings), .groups = "keep") %>%
-    mutate(productivity_loss_category = "premature death")
-  productivity_loss = rbind(productivity_loss,workshop)
-  #_______________________________________________________________________________
-  
-  
-  productivity_loss = productivity_loss %>%
+
+  productivity_loss = productivity_loss_breakdown %>%
     group_by(setting,booster_vax_scenario,intervention,intervention_target_group) %>%
     summarise(cost = sum(cost), .groups = "keep")
   
   return(productivity_loss)
+  #interestingly higher cost due to illness than death!
 }
