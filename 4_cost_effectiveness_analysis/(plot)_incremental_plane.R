d3669378889d336091f95adc86e4f2b23aa4aa62
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-07-05 15:23:52 +1000

    Create (plot)_incremental_plane.R

---
+++ 4_cost_effectiveness_analysis/(plot)_incremental_plane.R
@@ -0,0 +1,61 @@
+
+#Require result (CommandDeck_result_long) with variables outcome, setting, perspective, discounting_rate, antiviral_cost, booster_vax_scenario,antiviral_scenario,netCost,count_outcomes_averted
+
+INPUT_outcome = "QALYs"
+INPUT_setting_list = c("PNG")
+INPUT_perspective = "societal"
+INPUT_discounting_rate = 0.03
+INPUT_antiviral_cost = "middle_income_cost"
+INPUT_booster_strategy = "booster to all high-risk adults previously willing to be vaccinated"
+INPUT_antiviral_strategy = c("no antiviral","nirmatrelvir_ritonavir 2023-01-01 adults_with_comorbidities")
+
+options(scipen = 1000)
+
+### Check inputs
+if(length(INPUT_antiviral_strategy)>1 & length(INPUT_booster_strategy)>1){
+  stop('Please select either multiple antiviral strategies OR multiple booster strategies')
+}
+
+### Subset results
+to_plot = CommandDeck_result_long %>%
+  filter(outcome == INPUT_outcome &
+           setting %in% INPUT_setting_list &
+           perspective == INPUT_perspective &
+           discounting_rate == INPUT_discounting_rate &
+           antiviral_cost == INPUT_antiviral_cost &
+           booster_vax_scenario %in% INPUT_booster_strategy &
+           antiviral_scenario %in% INPUT_antiviral_strategy)
+
+
+### Create plots
+plot_list = list()
+for (i in 1:length(INPUT_setting_list)){
+  if (length(INPUT_antiviral_strategy)>1){
+    plot_list[[length(plot_list)+1]] = ggplot(to_plot) +
+      geom_point(aes(x=netCost,y=count_outcomes_averted,color=as.factor(antiviral_scenario))) +
+      labs(color="antiviral strategy") 
+  } else if (length(INPUT_booster_strategy)>1){
+    plot_list[[length(plot_list)+1]] = ggplot(to_plot) +
+      geom_point(aes(x=netCost,y=count_outcomes_averted,color=as.factor(booster_vax_scenario))) +
+      labs(color="booster strategy")
+  } else{
+    plot_list[[length(plot_list)+1]] = ggplot(to_plot) +
+      geom_point(aes(x=netCost,y=count_outcomes_averted)) 
+  }
+  plot_list[[length(plot_list)]] = plot_list[[length(plot_list)]] +
+    ylab("QALYs averted") +
+    xlab("net cost (2022 USD)") +
+    theme_bw() + theme(legend.position="bottom")
+
+}
+
+### Arrange plots based no number of settings
+if (length(plot_list) == 1){
+  plot_list
+} else if (length(plot_list) == 2){
+  ggarrange(plot_list, ncol = 1, nrow = 2, common.legend = TRUE)
+} else if (length(plot_list) == 3){
+  ggarrange(plot_list, ncol = 2, nrow = 2, common.legend = TRUE)
+} else if (length(plot_list) == 4){
+  ggarrange(plot_list, ncol = 2, nrow = 2, common.legend = TRUE)
+}
