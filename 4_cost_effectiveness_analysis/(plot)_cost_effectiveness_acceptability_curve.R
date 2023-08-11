e01d0b902042e553ff2616b6896b9a668b0da78f
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-08-11 15:14:15 +1000

    marry static back to dynamic WTP curve

--- 4_cost_effectiveness_analysis/(plot)_cost_effectiveness_acceptability_curve.R
+++ 4_cost_effectiveness_analysis/(plot)_cost_effectiveness_acceptability_curve.R
@@ -1,96 +1,122 @@
 options(scipen = 1000); require(ggpubr)
 
 
-INPUT_include_setting = c("Papua New Guinea")
+INPUT_include_setting = c("Fiji","Indonesia","Papua New Guinea","Timor-Leste")
 INPUT_include_outcome = "QALYs"
 INPUT_perspective = "healthcare perspective"
-
-INPUT_parameter_to_vary = "booster_strategy" 
-
-#R SHINY conditional UI, check boxes for one and button for all others
 INPUT_discounting_rate = 3
-INPUT_antiviral_cost_scenario = "low_generic_cost"
-INPUT_include_booster_vax_scenario = "high risk adults"
-# [1] "all adults who have previously completed their primary schedule but have not recieved a booster"      
-# [2] "high-risk adults who have previously completed their primary schedule but have not recieved a booster"
-# [3] "all adults"                                                                                           
-# [4] "high risk adults"                                                                                     
-# [5] "no booster
-INPUT_include_antiviral_type = c("nirmatrelvir_ritonavir")
-INPUT_include_antiviral_type = c(INPUT_include_antiviral_type,"no antiviral")
+INPUT_antiviral_cost_scenario = c("low_generic_cost","middle_income_cost","high_income_cost")
+INPUT_include_booster_vax_scenario = c("all adults","high risk adults","no booster")
+INPUT_antiviral_type = c("nirmatrelvir_ritonavir")
+INPUT_antiviral_type = c(INPUT_include_antiviral_type,"no antiviral")
 INPUT_include_antiviral_target_group = c("adults with comorbidities")
+INPUT_fix_xaxis = TRUE
+INPUT_include_GDP = TRUE
 
+
+
+### load functions
+subset_data_to_widgets <- function(df){
+  df %>%
+    filter(
+      setting %in% INPUT_include_setting &
+        perspective %in% INPUT_perspective  &
+        discounting_rate %in% INPUT_discounting_rate &
+        antiviral_cost_scenario %in% INPUT_antiviral_cost_scenario &
+        booster_vax_scenario %in% INPUT_include_booster_vax_scenario &
+        antiviral_target_group %in% INPUT_include_antiviral_target_group &
+        antiviral_type %in% c("no antiviral", INPUT_antiviral_type)
+    )
+}
+consolidate_plot_list <- function(plot_list){
+  if(length(plot_list) == 1){row_num = 1; col_num = 1}
+  if(length(plot_list) == 2){row_num = 1; col_num = 2}
+  if(length(plot_list) > 2) {row_num = 2; col_num = 2}
+  plot = ggarrange(plotlist = plot_list, ncol = col_num, nrow = row_num, common.legend = TRUE, legend = "bottom")
+}
+apply_plot_dimensions <- function(df,aes_x,aes_y,plot_dimensions){
+  
+  if (length(plot_dimensions) == 0){
+    this_plot = ggplot(df) +
+      geom_point(aes(x = .data[[aes_x]],y=.data[[aes_y]]))
+  } else if (length(plot_dimensions) == 1){
+    this_plot = ggplot(df) +
+      geom_point(aes(x = .data[[aes_x]],y=.data[[aes_y]],color=as.factor(.data[[plot_dimensions[1]]]))) +
+      labs(color = paste(gsub("_"," ", plot_dimensions[1])))
+  } else if (length(plot_dimensions) == 2){
+    if (aes_x == "WTP" & plot_dimensions[2] == "booster_vax_scenario"){
+      this_plot = ggplot(df) +
+        geom_point(aes(x = .data[[aes_x]],y=.data[[aes_y]],color=as.factor(.data[[plot_dimensions[1]]]))) +
+        labs(color = paste(gsub("_"," ", plot_dimensions[1]))) +
+        facet_grid(.data[[plot_dimensions[2]]]~.)
+    } else{
+      this_plot = ggplot(df) +
+        geom_point(aes(x = .data[[aes_x]],y=.data[[aes_y]],color=as.factor(.data[[plot_dimensions[1]]]),shape = as.factor(.data[[plot_dimensions[2]]]))) +
+        labs(color = paste(gsub("_"," ", plot_dimensions[1])),
+             shape = paste(gsub("_"," ", plot_dimensions[2])))
+    }
+  }
+  
+  return(this_plot)
+}
+plot_dimensions <- function(INPUT_antiviral_cost_scenario,INPUT_discounting_rate,INPUT_include_antiviral_target_group,INPUT_include_booster_vax_scenario){
+  plot_dimension_vector = c()
+  
+  if (length(INPUT_antiviral_cost_scenario)>1)       {plot_dimension_vector = c(plot_dimension_vector,"antiviral_cost_scenario")}
+  if (length(INPUT_discounting_rate)>1)              {plot_dimension_vector = c(plot_dimension_vector,"discounting_rate")}
+  if (length(INPUT_include_antiviral_target_group)>1){plot_dimension_vector = c(plot_dimension_vector,"antiviral_target_group")}
+  if (length(INPUT_include_booster_vax_scenario)>1)  {plot_dimension_vector = c(plot_dimension_vector,"booster_vax_scenario")}
+  
+  plot_dimension_vector
+}
 
 
 ### Create plots
-this_workshop = CEAC_dataframe %>% filter(outcome == INPUT_include_outcomes &
-                                            setting %in% INPUT_include_setting &
-                                            perspective == INPUT_perspective &
-                                            discounting_rate == INPUT_discounting_rate &
-                                            antiviral_cost_scenario == INPUT_antiviral_cost_scenario &
-                                            booster_vax_scenario %in% INPUT_include_booster_vax_scenario &
-                                            antiviral_type %in% INPUT_include_antiviral_type & 
-                                            antiviral_target_group %in% INPUT_include_antiviral_target_group)
-xmax = this_workshop %>% 
-  group_by(setting,booster_vax_scenario,antiviral_target_group) %>%
-  filter(round(probability,digits=2) >=0.99) %>%
-  summarise(min = min(WTP), .groups = "keep") %>%
-  ungroup()
-xmax = max(xmax$min)
+to_plot = subset_data_to_widgets(CEAC_dataframe)%>%
+  filter(outcome %in% INPUT_include_outcomes) 
 
-xmin = this_workshop %>% 
-  group_by(setting,booster_vax_scenario,antiviral_target_group) %>%
-  filter(probability == min(probability)) %>%
-  summarise(max = max(WTP), .groups = "keep") %>%
-  ungroup()
-xmin = min(xmin$max)
+if(INPUT_fix_xaxis == TRUE){
+  to_plot_xmin = min(to_plot$WTP)
+  to_plot_xmax = max(to_plot$WTP)
+}
 
-n_options_selected = max(length(INPUT_discounting_rate),
-                         length(INPUT_antiviral_cost_scenario),
-                         length(INPUT_include_booster_vax_scenario),
-                         length(INPUT_include_antiviral_target_group))
 
-plot_list = list()
-for (this_setting in INPUT_include_setting){
-  to_plot_setting = this_workshop[this_workshop$setting == this_setting,]
-  if (length(INPUT_include_antiviral_target_group)>1){
-    plot_list[[length(plot_list)+1]] = ggplot(to_plot_setting) +
-      geom_point(aes(x=WTP,y=probability,color=as.factor(antiviral_target_group))) +
-      labs(color="antiviral strategy") 
-  } else if (length(INPUT_include_booster_vax_scenario)>1){
-    plot_list[[length(plot_list)+1]] = ggplot(to_plot_setting) +
-      geom_point(aes(x=WTP,y=probability,color=as.factor(booster_vax_scenario))) +
-      labs(color="booster strategy")
-  } else if (length(INPUT_discounting_rate)>1){
-    plot_list[[length(plot_list)+1]] = ggplot(to_plot_setting) +
-      geom_point(aes(x=WTP,y=probability,color=as.factor(discounting_rate))) +
-      labs(color="discounting rate")
-  } else if (length(INPUT_antiviral_cost_scenario)>1){
-    plot_list[[length(plot_list)+1]] = ggplot(to_plot_setting) +
-      geom_point(aes(x=WTP,y=probability,color=as.factor(antiviral_cost_scenario))) +
-      labs(color="antiviral cost")
-  } else{
-    plot_list[[length(plot_list)+1]] = ggplot(to_plot_setting) +
-      geom_point(aes(x=WTP,y=probability)) 
+if (nrow(to_plot) > 1) {
+  plot_list = list()
+  
+  for (this_setting in INPUT_include_setting) {
+    
+    if (this_setting == "Fiji"){this_setting_GDP = 5316.7
+    } else if (this_setting == "Indonesia"){this_setting_GDP = 4788.0
+    } else if (this_setting == "Papua New Guinea"){this_setting_GDP = 3020.3
+    } else if (this_setting == "Timor-Leste"){this_setting_GDP = 2358.4}
+    
+    plot_list[[length(plot_list)+ 1]] = apply_plot_dimensions(df = to_plot[to_plot$setting == this_setting,],
+                                                              aes_x="WTP",
+                                                              aes_y="probability",
+                                                              plot_dimensions = plot_dimensions(INPUT_antiviral_cost_scenario,INPUT_discounting_rate,INPUT_include_antiviral_target_group,INPUT_include_booster_vax_scenario))  +
+      xlab(paste("Willingness to pay ($/",INPUT_include_outcomes,")",sep="")) +
+      ylab("Probability cost-effective") +
+      theme_bw() +
+      theme(legend.position = "bottom") +
+      labs(title = this_setting)  +
+      guides(color = guide_legend(ncol = 1),shape = guide_legend(ncol = 1)) +
+      scale_color_manual(values = wesanderson::wes_palette( name="FantasticFox1"))
+    
+    if(INPUT_fix_xaxis == TRUE){
+      plot_list[[length(plot_list)]] =  plot_list[[length(plot_list)]] +
+        xlim(to_plot_xmin,to_plot_xmax)
+    }
+    if (INPUT_include_GDP == TRUE){
+      plot_list[[length(plot_list)]] = plot_list[[length(plot_list)]] + 
+        geom_vline(mapping = NULL, xintercept = this_setting_GDP, linetype='dashed') #+
+      #annotate("text", y = 0.25, x = this_setting_GDP*annotate_multiple, label = "GDP per capita", angle = 90) 
+      #very difficult NOT to get text to overlap something with range of WTP
+    }
   }
-  plot_list[[length(plot_list)]] = plot_list[[length(plot_list)]] +
-    xlab("Willingness to pay ($/QALY)") +
-    ylab("Probability cost-effective") +
-    theme_bw() + 
-    theme(legend.position="bottom") +
-    labs(title = this_setting) +
-    xlim(xmin,xmax) + guides(colour = guide_legend(nrow = n_options_selected))
+
+  #rm(to_plot)
+  print(consolidate_plot_list(plot_list))
 }
 
 
-
-### Arrange plots based no number of settings
-if (length(plot_list) == 1){
-  plot_list
-} else if (length(plot_list) == 2){
-  ggarrange(plot_list[[1]],plot_list[[2]], ncol = 1, nrow = 2, common.legend = TRUE)
-} else if (length(plot_list) == 3){
-  ggarrange(plot_list[[1]],plot_list[[2]],plot_list[[3]], ncol = 2, nrow = 2, common.legend = TRUE)
-} else if (length(plot_list) == 4){
-  ggarrange(plot_list[[1]],plot_list[[2]],plot_list[[3]],plot_list[[4]], ncol = 2, nrow = 2, common.legend = TRUE)
-}
