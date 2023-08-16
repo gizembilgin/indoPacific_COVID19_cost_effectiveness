53531bafa8c3a16d0852a534d919f17f75c70f3f
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-08-16 12:49:44 +1000

    creating table and figure for the impact of long COVID on the cost-effectiveness of booster doses

--- 4_cost_effectiveness_analysis/(plot)_incremental_plane.R
+++ 4_cost_effectiveness_analysis/(plot)_incremental_plane.R
@@ -1,8 +1,10 @@
 #Require result (CommandDeck_result_long) with variables outcome, setting, perspective, discounting_rate, antiviral_cost_scenario, booster_vax_scenario,antiviral_type,netCost,count_outcomes
 require(ggpubr);options(scipen = 1000)
 
+INPUT_plot_name = "long_COVID" # options: static_plot, reduced_static_plot (hosted R Shiny), long_COVID (SM S4.7)
 
-INPUT_include_setting = c("Papua New Guinea","Timor-Leste","Fiji","Indonesia")
+
+INPUT_include_setting = c("Fiji","Indonesia","Papua New Guinea","Timor-Leste")
 INPUT_include_booster_vax_scenario = c("high risk adults"
                                        , "all adults"
                                        , "all adults (catch-up campaign)"
@@ -11,10 +13,45 @@ INPUT_include_booster_vax_scenario = c("high risk adults"
 INPUT_antiviral_type = c() # c("nirmatrelvir_ritonavir")
 INPUT_antiviral_type = c(INPUT_antiviral_type,"no antiviral")
 INPUT_include_antiviral_target_group = c("no antiviral")
-INPUT_perspective = "societal perspective"
+INPUT_perspective = c("healthcare perspective","societal perspective")
 INPUT_discounting_rate = 3
-INPUT_antiviral_cost_scenario = "middle_income_cost"
+INPUT_antiviral_cost_scenario = "middle-income reference price ($250 USD per schedule)"
 INPUT_include_outcomes = "QALYs"
+
+# INPUT_include_booster_vax_scenario = "no booster"
+# INPUT_antiviral_type = c("nirmatrelvir_ritonavir")
+# INPUT_include_antiviral_target_group = c("adults with comorbidities","all adults","unvaccinated adults")
+
+### load data
+if (INPUT_plot_name %in% c("static_plot","reduced_static_plot")){
+  if (INPUT_plot_name == "static_plot"){
+    this_pattern = "CommandDeck_result_long_1_*"
+  } else if (INPUT_plot_name == "reduced_static_plot"){
+    this_pattern = "CommandDeck_result_long_reduced_*"
+  }
+  
+  list_poss_Rdata = list.files(path = "Rshiny/x_results/",pattern = this_pattern)
+  if (length(list_poss_Rdata) > 0) {
+    list_poss_Rdata_details = double()
+    for (j in 1:length(list_poss_Rdata)) {
+      list_poss_Rdata_details = rbind(list_poss_Rdata_details,
+                                      file.info(paste("Rshiny/x_results/", list_poss_Rdata[[j]], sep = ''))$mtime)
+    }
+    latest_file = list_poss_Rdata[[which.max(list_poss_Rdata_details)]]
+    load(file = paste("Rshiny/x_results/", latest_file, sep = ''))
+    
+    #if part1, also load part2 and join
+    if (this_pattern == "CommandDeck_result_long_1_*"){
+      load(file = paste("Rshiny/x_results/", gsub("long_1","long_2",latest_file), sep = ''))
+      CommandDeck_result_long = rbind(CommandDeck_result_long_part1,CommandDeck_result_long_part2); rm(CommandDeck_result_long_part1,CommandDeck_result_long_part2)
+    }
+  } else{
+    stop(paste("no results to load!"))
+  }
+} else if (INPUT_plot_name == "long_COVID"){
+  load(file = "Rshiny/x_results/long_COVID_results.Rdata")
+  CommandDeck_result_long = long_COVID_results
+}
 
 
 ### load functions
@@ -42,6 +79,11 @@ apply_plot_dimensions <- function(df,aes_x,aes_y,plot_dimensions){
     this_plot = ggplot(df) +
       geom_point(aes(x = .data[[aes_x]],y=.data[[aes_y]]))
   } else if (length(plot_dimensions) == 1){
+    
+    if(INPUT_plot_name == "long_COVID"){
+      df = df %>%
+        mutate("{plot_dimensions[1]}" := paste0(.data[[plot_dimensions[1]]]," (long COVID ",long_COVID,")"))
+    }
     this_plot = ggplot(df) +
       geom_point(aes(x = .data[[aes_x]],y=.data[[aes_y]],color=as.factor(.data[[plot_dimensions[1]]]))) +
       labs(color = paste(gsub("_"," ", plot_dimensions[1])))
@@ -51,12 +93,17 @@ apply_plot_dimensions <- function(df,aes_x,aes_y,plot_dimensions){
       labs(color = paste(gsub("_"," ", plot_dimensions[1])),
            shape = paste(gsub("_"," ", plot_dimensions[2])))
   }
+  if (length(INPUT_perspective)>1){
+    this_plot = this_plot + 
+      facet_grid(perspective ~.) 
+  }
   
   return(this_plot)
 }
 plot_dimensions <- function(INPUT_antiviral_cost_scenario,INPUT_discounting_rate,INPUT_include_antiviral_target_group,INPUT_include_booster_vax_scenario){
   plot_dimension_vector = c()
   
+  #if(INPUT_plot_name == "long_COVID"){plot_dimension_vector = c(plot_dimension_vector,"long_COVID")}
   if (length(INPUT_antiviral_cost_scenario)>1)       {plot_dimension_vector = c(plot_dimension_vector,"antiviral_cost_scenario")}
   if (length(INPUT_discounting_rate)>1)              {plot_dimension_vector = c(plot_dimension_vector,"discounting_rate")}
   if (length(INPUT_include_antiviral_target_group)>1){plot_dimension_vector = c(plot_dimension_vector,"antiviral_target_group")}
@@ -73,12 +120,17 @@ to_plot =  subset_data_to_widgets(CommandDeck_result_long)%>%
 ### Create plots
 plot_list = list()
 for (this_setting in INPUT_include_setting){
-  plot_list[[length(plot_list)+ 1]] = apply_plot_dimensions(df = to_plot[to_plot$setting == this_setting,],
+  workshop =  to_plot[to_plot$setting == this_setting,]
+  plot_list[[length(plot_list)+ 1]] = apply_plot_dimensions(df = workshop,
                                                             aes_x="netCost",
                                                             aes_y="count_outcomes",
                                                             plot_dimensions = plot_dimensions(INPUT_antiviral_cost_scenario,INPUT_discounting_rate,INPUT_include_antiviral_target_group,INPUT_include_booster_vax_scenario))  +
     ylab(paste(INPUT_include_outcomes,"averted")) +
     xlab("incremental cost (2022 USD)") +
+    xlim(min(min(workshop$netCost),0), 
+         max(max(workshop$netCost),0)) + 
+    ylim(min(min(workshop$count_outcomes),0), 
+         max(max(workshop$count_outcomes),0))  +
     theme_bw() +
     theme(legend.position="bottom") +
     labs(title = this_setting) +
@@ -94,6 +146,30 @@ for (this_setting in INPUT_include_setting){
 }
 
 print(consolidate_plot_list(plot_list))
+
+#let's create a table to summarise the impact of long COVID on the cost-effectiveness of booster doses
+if (INPUT_plot_name == "long_COVID"){
+  to_plot %>% 
+    group_by(evaluation_level, perspective, discounting_rate,setting,booster_vax_scenario,antiviral_cost_scenario,
+             antiviral_type,antiviral_target_group,long_COVID,outcome) %>%
+    summarise(
+      #NB: costs don't change!
+      #expected_interventionCost = mean(interventionCost),           #long COVID doesn't impact the cost of the booster doses
+      #expected_healthcareCostAverted = mean(healthcareCostAverted), #long COVID doesn't impact immediate healthcare needs of severe cases
+      #expected_productivityLoss = mean(productivityLoss),           #we haven't included any productivity losses associated with long COVID
+      #expected_netCost = mean(netCost),
+      expected_count_outcome = mean(count_outcomes),
+              .groups = "keep") %>%
+    ungroup() %>%
+    #outcomes averted are not influenced by the CEA perspective
+    filter(perspective == "healthcare perspective") %>%
+    select(-perspective,-evaluation_level,-antiviral_cost_scenario,-discounting_rate,-antiviral_type,-antiviral_target_group,-outcome) %>%
+    pivot_wider(values_from = expected_count_outcome,
+                names_from = long_COVID,
+                names_prefix = "long_COVID_") 
+
+}
+
 
 
 ### make catch-up campaign a shape
---
+++ 4_cost_effectiveness_analysis/(run)_impact_of_longCOVID.R
@@ -0,0 +1,88 @@
+### RUN SCENARIOS FOR SUPPLEMENTARY MATERIAL S4.6
+
+
+queue = list(
+  list(TOGGLE_longCOVID = "off"),
+  list(TOGGLE_longCOVID = "on")
+)
+
+long_COVID_results = data.frame()
+CommandDeck_CONTROLS = list()
+
+for (ticket in 1:length(queue)){
+  CommandDeck_CONTROLS = queue[[ticket]]
+  CommandDeck_CONTROLS = append(CommandDeck_CONTROLS,
+                                list(
+                                  LIST_CEA_settings = list("PNG_low_beta","TLS","FJI","IDN"),
+                                  LIST_perspectives = c("healthcare","societal"),
+                                  LIST_antiviral_cost_scenario = c("low_generic_cost","middle_income_cost", "high_income_cost"),
+                                  LIST_discounting_rate = 0.03,
+                                  
+                                  LIST_booster_vax_scenarios = list(
+                                    "all willing adults vaccinated with a primary schedule and high risk group recieve a booster: assume booster to all adults who have previously recieved a primary schedule"
+                                    ,"all willing adults vaccinated with a primary schedule plus booster dose: assume booster to all adults who have previously recieved a primary schedule"                    
+                                    ,"all willing adults vaccinated with a primary schedule"  
+                                  ),
+                                  LIST_antiviral_elig_groups = list("adults_with_comorbidities"),
+                                  LIST_antiviral_types = list("nirmatrelvir_ritonavir"),
+                                  
+                                  TOGGLE_uncertainty = "rand",
+                                  TOGGLE_numberOfRuns = 1000, 
+                                  TOGGLE_clusterNumber = 3,
+                                  DECISION_include_net = "N",
+                                  DECISION_sampling_strategy = "single_run"
+                                )
+  )
+  
+  source(paste(getwd(),"/CommandDeck.R",sep=""))
+  
+  rows = CommandDeck_result_long  %>%
+    mutate(long_COVID = CommandDeck_CONTROLS$TOGGLE_longCOVID) 
+  
+  long_COVID_results = rbind(long_COVID_results,rows)
+  
+}
+
+CommandDeck_CONTROLS = list()
+
+long_COVID_results = long_COVID_results %>%
+  mutate(
+    setting = case_when(
+      setting == "FJI" ~ "Fiji",
+      setting == "IDN" ~ "Indonesia",
+      setting == "PNG" ~ "Papua New Guinea",
+      setting == "TLS" ~ "Timor-Leste",
+      TRUE ~ setting
+    ),
+    
+    booster_vax_scenario = case_when(
+      booster_vax_scenario == "booster to all high-risk adults previously willing to be vaccinated" ~ "high risk adults",
+      booster_vax_scenario == "booster to all adults previously willing to be vaccinated" ~ "all adults"    ,
+      booster_vax_scenario == "booster dose catch-up campaign for all adults" ~ "all adults who have previously completed their primary schedule but have not recieved a booster"  ,
+      booster_vax_scenario == "booster dose catch-up campaign for high-risk adults" ~ "high-risk adults who have previously completed their primary schedule but have not recieved a booster"   ,
+      booster_vax_scenario == "no booster dose" ~ "no booster",
+      TRUE ~ booster_vax_scenario
+    ),
+    
+    antiviral_type = gsub(" 2023-01-01","",antiviral_type),
+    antiviral_target_group = gsub("_"," ",antiviral_target_group),
+    antiviral_target_group = case_when(
+      antiviral_type == "no antiviral" ~ antiviral_type,
+      TRUE ~ antiviral_target_group
+    ),
+    
+    perspective = paste(perspective," perspective",sep = ""),
+    
+    antiviral_cost_scenario = 
+      case_when(antiviral_cost_scenario == "high_income_cost" ~ "high-income reference price ($530 USD per schedule)",
+                antiviral_cost_scenario == "middle_income_cost" ~ "middle-income reference price ($250 USD per schedule)",
+                antiviral_cost_scenario == "low_generic_cost" ~ "low generic reference price ($25 USD per schedule)"),
+    
+    discounting_rate = discounting_rate *100
+  )
+
+long_COVID_results$antiviral_cost_scenario <- factor(long_COVID_results$antiviral_cost_scenario, levels = rev(c("low generic reference price ($25 USD per schedule)",
+                                                                                                                              "middle-income reference price ($250 USD per schedule)",
+                                                                                                                              "high-income reference price ($530 USD per schedule)")))
+save(long_COVID_results,file = "Rshiny/x_results/long_COVID_results.Rdata")
+#_______________________________________________________________________________
